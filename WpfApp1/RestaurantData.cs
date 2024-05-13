ad7dab69f3466658db51fe23cb7778d97e4a70c1
James Mccafferty Devers <123546822+JamDeverATU@users.noreply.github.com>
2024-05-13 13:17:08 +0100

    f

---
+++ WpfApp1/RestaurantData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class RestaurantData : DbContext
+    {
+        public RestaurantData(string OODExam_JamesMcaffertyDevers) : base(OODExam_JamesMcaffertyDevers) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+    }
+}
